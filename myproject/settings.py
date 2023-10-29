773c003c633e5fc1f52da8ad66fca13b9318183e
faysal <faysalhassantorjo8@gmail.com>
2023-10-29 14:55:49 +0600

    commit 2

--- myproject/settings.py
+++ myproject/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-1(fwm($trg_0(w^p@wunjcazn&7u__kejd_!07arw#f7z!)!6f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '*']
+ALLOWED_HOSTS = ['*']
 
 
 
@@ -129,6 +129,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # django_heroku.settings(locals())
