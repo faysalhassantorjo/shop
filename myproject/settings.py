873c6213057d9e8b0e43a51c78989f5437a66154
faysal <faysalhassantorjo8@gmail.com>
2023-12-03 00:59:05 +0600

    commit 4

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
